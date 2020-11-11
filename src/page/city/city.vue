b5f74a7151b5744d399f8b971cb64a1b1d67df76
iam <3275897688@qq.com>
2020-11-11 20:54:53 +0800

    city完成未测试

--- src/page/city/city.vue
+++ src/page/city/city.vue
@@ -101,3 +101,70 @@
 	}
 </script>
 
+<style lang='stylus'>
+@import '../../style/mixin'
+  
+.city_container
+  padding-top 2.rem
+
+.change_city
+  right 0.4rem
+  sc(0.6rem, #fff);
+  ct();
+  
+.city_form
+  background-color #fff
+  border-top 1px solid $bc
+  border-bottom 1px solid $bc
+  padding-top 0.4rem
+  div
+    width 90%
+    margin 0 auto 
+    text-align center
+    
+.city_input
+  border 1px solid $bc
+  padding 0 0.3rem
+  sc(0.65rem, #fff);
+.input_style
+  border-radius 0.1rem
+  margin-bottom 0.4rem
+  wh(100%, 1.4rem);
+.city_submit
+  background-color $blue
+  sc(0.65rem, #fff);
+
+.pois_search_history
+  border-top 1px solid $bc
+  border-bottom 1px solid $bc
+  padding-left 0.5rem
+  font(0.475rem, 0.8rem)
+
+.getpois_ul
+  background-color #fff
+  border-top 1px solid $bc
+  li
+    margin 0 auto
+    padding-top 0.65rem
+    border-bottom 1px solid $bc
+
+.pois_name
+  margin 0 auto 0.35rem
+  width 90%
+  sc(0.65rem, #333);
+.pois_address
+  width 90%
+  margin 0 auto 0.55rem
+  sc(0.45rem, #999);
+.search_none_place
+  margin 0 auto
+  font(0.65rem, 1.75rem);
+  color #333333
+  background-color #FFFFFF
+  text-indent 0.5rem
+.clear_all_history
+  sc(0.7rem, #666);
+  text-align center
+  line-height 2rem
+  background-color #FFFFFF
+</style>
